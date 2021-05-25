00250c833882ecabc051e6756171aa503e71cccc
kai-hsieh <mister33221@gmail.com>
2021-05-26 03:35:03 +0800

    blank

---
+++ shopping_cart_test_Artifact_Id/src/main/java/shopping_cart_test/App.java
@@ -0,0 +1,13 @@
+package shopping_cart_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
